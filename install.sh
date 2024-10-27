ef67a73df57f683e0fbb352c835803297513ca55
Retochan <100085949+GiganticDoor136@users.noreply.github.com>
2024-10-28 01:00:21 +0900

    install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 echo "installing duckpack, for using the duckpack, use command duck, example: duck pack neofetch"
-chmod +x duckpack
-alias duck='/pm/duckpack/duckpack' 
+sudo chmod +x duckpack
+sudo alias duck='/pm/duckpack/duckpack' 
 sudo rm -rf /pm/duckpack/install.sh
+echo "Complete!"
